3afec5ce35f1d3a2e401f8b61c8a83dc352fc358
snorkungen <teuvo2004@gamil.com>
2023-01-18 16:23:32 +0200

    test: added tests

--- test/transformers.test.ts
+++ test/transformers.test.ts
@@ -1,4 +1,5 @@
 import UNSMART_JSON from "../src/index"
+import type { TTransformer } from "../src/transformer";
 
 const OBJECT_WITH_DATE = {
     date_first: new Date("2023-01-15T12:08:46.000Z"),
@@ -33,7 +34,6 @@ const OBJECT_WITH_URL = {
 let set2 = new Set();
 set2.add(1)           // Set(1) { 1 }
 set2.add(5)           // Set(2) { 1, 5 }
-set2.add(5)           // Set(2) { 1, 5 }
 set2.add('some text') // Set(3) { 1, 5, 'some text' }
 const o = { a: 1, b: 2 }
 set2.add(o)
@@ -44,34 +44,70 @@ const OBJECT_WITH_SET = {
 }
 
 describe("Test default transformers", () => {
-    let stringified_date = UNSMART_JSON.stringify(OBJECT_WITH_DATE);
-    let parsed_date = UNSMART_JSON.parse(stringified_date);
-
     test("Works with Date", () => {
-        expect(parsed_date).toEqual(OBJECT_WITH_DATE)
+        expect(UNSMART_JSON.parse(
+            UNSMART_JSON.stringify(OBJECT_WITH_DATE)
+        )).toStrictEqual(OBJECT_WITH_DATE)
     });
 
     test("Works with Regex", () => {
         expect(UNSMART_JSON.parse(
             UNSMART_JSON.stringify(OBJECT_WITH_REGEX)
-        )).toEqual(OBJECT_WITH_REGEX)
+        )).toStrictEqual(OBJECT_WITH_REGEX)
     });
 
     test("Works with Map", () => {
         expect(UNSMART_JSON.parse(
             UNSMART_JSON.stringify(OBJECT_WITH_MAP)
-        )).toEqual(OBJECT_WITH_MAP)
+        )).toStrictEqual(OBJECT_WITH_MAP)
     });
 
     test("Works with URL", () => {
         expect(UNSMART_JSON.parse(
             UNSMART_JSON.stringify(OBJECT_WITH_URL)
-        )).toEqual(OBJECT_WITH_URL)
+        )).toStrictEqual(OBJECT_WITH_URL)
     });
 
     test("Works with Set", () => {
         expect(UNSMART_JSON.parse(
             UNSMART_JSON.stringify(OBJECT_WITH_SET)
-        )).toEqual(OBJECT_WITH_SET)
+        )).toStrictEqual(OBJECT_WITH_SET)
+    });
+})
+
+describe("Test createTransformer", () => {
+    const { createTransformer, transformers } = UNSMART_JSON;
+    const TRANSFORMER_NAME = "test-name-for-transformer";
+
+    class TransformMe {
+        constructor(public first: string, public last: string) { }
+
+        get name() {
+            return this.first + " " + this.last;
+        }
+    }
+
+    const TEST_DATA = {
+        hello: "world",
+        tt: new TransformMe("jj","kk")
+    }
+
+    const transformer: TTransformer<TransformMe, string> = {
+        test: (val) => val instanceof TransformMe,
+        serialize: (val) => val.name,
+        deserialize: (val) => new TransformMe(val.split(" ")[0], val.split(" ")[1])
+    };
+
+    // Add transformer to transformers
+    createTransformer(TRANSFORMER_NAME, transformer);
+
+    test("created transformer gets added with transformers", () => {
+        expect(transformers[TRANSFORMER_NAME]).toBe(transformer);
+    });
+
+    test("Transformer transforms data", () => {
+        expect(UNSMART_JSON.parse(
+            UNSMART_JSON.stringify(TEST_DATA)
+        )).toStrictEqual(TEST_DATA);
     });
 })
